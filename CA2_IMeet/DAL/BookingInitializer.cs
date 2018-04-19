83627322bbb8281f358329a3bb3274a92418cebd
Carole vd <Carole.vd@gmail.com>
2018-04-19 22:33:17 +0100

    Added BookingInitializer to Drop Database when Model Changes

---
+++ CA2_IMeet/DAL/BookingInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CA2_IMeet.DAL
+{
+    public class BookingInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<BookingContext>
+    {
+        protected override void Seed(BookingContext context)
+        {
+        }
+    }
+}
