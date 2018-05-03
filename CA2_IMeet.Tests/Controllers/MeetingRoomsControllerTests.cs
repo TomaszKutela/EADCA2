3d1e314067edabcb4c47116ed4d9f211c4654153
suejones <suemileyjones@gmail.com>
2018-05-03 17:51:47 +0100

    Draft Meeting Room Comtroller Tests

--- CA2_IMeet.Tests/Controllers/MeetingRoomsControllerTests.cs
+++ CA2_IMeet.Tests/Controllers/MeetingRoomsControllerTests.cs
@@ -1,14 +1,60 @@
 ﻿using System;
+using System.Web.Mvc;
+using CA2_IMeet.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CA2_IMeet.Tests.Controllers
 {
+    /* Draft Meeting Room Contoller Tests
     [TestClass]
     public class MeetingRoomsControllerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Index(string sortOrder, string searchString)
         {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
         }
+
+        [TestMethod]
+        public void Details(string keyword)
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+        [TestMethod]
+        public void Create()
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+        [TestMethod]
+        public void Create([Bind(Include = "Name,Size,Location,Equipment")] Models.MeetingRoom meetingRoom)
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+
+        [TestMethod]
+        public void Edit(int? id)
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+
+        [TestMethod]
+        public void EditPost(int? id)
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+
+        [TestMethod]
+        public void Delete(int? id, bool? saveChangesError = false)
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+
+        [TestMethod]
+        public void Delete(int id)
+        {
+            MeetingRoomsController meetroomController = new MeetingRoomsController;
+        }
+
+*/
     }
 }
