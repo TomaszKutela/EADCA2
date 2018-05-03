f481a40b148146175bdb97c4dc48b3d44fe5afe4
suejones <suemileyjones@gmail.com>
2018-05-03 17:31:40 +0100

    Draft Booking Controller Test template

--- CA2_IMeet.Tests/Controllers/BookingControllerTests.cs
+++ CA2_IMeet.Tests/Controllers/BookingControllerTests.cs
@@ -1,14 +1,65 @@
 ﻿using System;
+using System.Web.Mvc;
+using CA2_IMeet.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CA2_IMeet.Tests.Controllers
 {
     [TestClass]
     public class BookingControllerTests
-    {
+    {/* Work in Progress Test Methods
+
         [TestMethod]
-        public void TestMethod1()
+        public void ViewAllBookings()
         {
+            BookingController bookingController = new BookingController;
+
         }
+        [TestMethod]
+        public void Details(int? id)
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+
+        [TestMethod]
+        public void Create()
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+
+        [TestMethod]
+        public void Create([Bind(Include = "MeetingReference, RoomId, Date, Start_DateTime, End_DateTime")] Booking booking)
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+        [TestMethod]
+        public void Edit(int? id)
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+        [TestMethod]
+        public void EditPost(int? id)
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+        [TestMethod]
+        public void Delete(int? id, bool? saveChangesError = false)
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+        [TestMethod]
+        public void DeleteConfirmed(int id)
+        {
+            BookingController bookingController = new BookingController;
+
+        }
+        */
+
     }
 }
