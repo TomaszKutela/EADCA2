0de2f2a670f001cfd31cf1b76972417cf3560249
suejones <suemileyjones@gmail.com>
2018-05-03 15:12:07 +0100

    changes to bootstrap correct typos

---
+++ CA2_IMeet.Tests/Controllers/MeetingRoomsControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CA2_IMeet.Tests.Controllers
+{
+    [TestClass]
+    public class MeetingRoomsControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
