d1b2944732b4011b4de9ab4e73c451d6c52c721e
suejones <suemileyjones@gmail.com>
2018-05-04 21:13:38 +0100

    Update Booking Controller Tests

--- CA2_IMeet.Tests/Controllers/BookingControllerTests.cs
+++ CA2_IMeet.Tests/Controllers/BookingControllerTests.cs
@@ -12,51 +12,100 @@ namespace CA2_IMeet.Tests.Controllers
         [TestMethod]
         public void ViewAllBookings()
         {
-            BookingController bookingController = new BookingController;
+           
+            // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
         [TestMethod]
         public void Details(int? id)
         {
-            BookingController bookingController = new BookingController;
+            // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
 
         [TestMethod]
         public void Create()
         {
-            BookingController bookingController = new BookingController;
+            // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
 
         [TestMethod]
         public void Create([Bind(Include = "MeetingReference, RoomId, Date, Start_DateTime, End_DateTime")] Booking booking)
         {
-            BookingController bookingController = new BookingController;
+            // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
         [TestMethod]
         public void Edit(int? id)
         {
-            BookingController bookingController = new BookingController;
+           // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
         [TestMethod]
         public void EditPost(int? id)
         {
-            BookingController bookingController = new BookingController;
+            // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
         [TestMethod]
         public void Delete(int? id, bool? saveChangesError = false)
         {
-            BookingController bookingController = new BookingController;
+           // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
         [TestMethod]
         public void DeleteConfirmed(int id)
         {
-            BookingController bookingController = new BookingController;
+           // Arrange
+           BookingController bookingController = new BookingController();
+
+            // Act
+           
+
+            // Assert
 
         }
         */
