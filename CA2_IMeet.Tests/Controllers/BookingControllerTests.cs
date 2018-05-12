15bd7fcb12933486cf16536c90fc822ac3b0aa74
suejones <suemileyjones@gmail.com>
2018-05-12 15:17:24 +0100

    Working on Test files. Auto create ex controller & edit tests

--- CA2_IMeet.Tests/Controllers/BookingControllerTests.cs
+++ CA2_IMeet.Tests/Controllers/BookingControllerTests.cs
@@ -1,126 +1,182 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using CA2_IMeet.Controllers;
+using CA2_IMeet.DAL;
 using CA2_IMeet.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CA2_IMeet.Tests.Controllers
 {
     [TestClass]
-    public class BookingControllerTests
+    public class BookingControllerTests 
     {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateStep2Test()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePostTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditPostTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAvailableRoomsTest()
+        {
+            Assert.Fail();
+        }
         /*
-        [TestMethod]
-        public void ViewAllBookings()
-        {
-           // Arrange
-           BookingController bookingController = new BookingController();
-            
-           // Act
-            {
-                System.Collections.Generic.IEnumerable<Booking> bookingsToShow;
-            if (User.IsInRole("Admin"))
-            {
-                bookingsToShow = db.Bookings.Where(b => b.Date >= DateTime.Today).Include(b => b.MeetingRoom).OrderBy(b => b.Date).ThenBy(b => b.Start_DateTime).ToList();
-            }
-            else
-            {
-                string user_id = User.Identity.GetUserName();
-                bookingsToShow = db.Bookings.Where(b => b.UserId == user_id).Where(b => b.Date >= DateTime.Today).Include(b => b.MeetingRoom).OrderBy(b => b.Date).ThenBy(b => b.Start_DateTime).ToList();
-            }
-            // Assert
 
-            return View(bookingsToShow);
-        }
+[TestMethod]
+public void Index()
+{
+// Arrange
+BookingController bookingController = new BookingController();
 
-       
-        [TestMethod]
-        public void Details(int? id)
-        {
-            // Arrange
-           BookingController bookingController = new BookingController();
 
-            // Act
-           
+// Act
+{
+IEnumerable<Booking> bookingsToShow;
+if (User.IsInRole("Admin"))
+{
+bookingsToShow = db.Bookings.Where(b => b.Date >= DateTime.Today).Include(b => b.MeetingRoom).OrderBy(b => b.Date).ThenBy(b => b.Start_DateTime).ToList();
+}
+else
+{
+string user_id = User.Identity.GetUserName();
+bookingsToShow = db.Bookings.Where(b => b.UserId == user_id).Where(b => b.Date >= DateTime.Today).Include(b => b.MeetingRoom).OrderBy(b => b.Date).ThenBy(b => b.Start_DateTime).ToList();
+}
+// Assert
 
-            // Assert
+return View(bookingsToShow);
 
-        }
+}
 
-        [TestMethod]
-        public void Create()
-        {
-            // Arrange
-           BookingController bookingController = new BookingController();
+/*          // GET: Booking/Create
+//Used to Collect Criteria to Find Room
+public ActionResult Create()
+{
+PopulateStartTimeDropDownList();
+PopulateEndTimeDropDownList();
+return View();
+}
 
-            // Act
-           
+[TestMethod]
+public void Create()
+{
+// Arrange
+BookingController bookingController = new BookingController();
 
-            // Assert
+// Act
+PopulateStartTimeDropDownList();
+PopulateEndTimeDropDownList();
 
-        }
+// Assert
+return View();
 
-        [TestMethod]
-        public void Create([Bind(Include = "MeetingReference, RoomId, Date, Start_DateTime, End_DateTime")] Booking booking)
-        {
-            // Arrange
-           BookingController bookingController = new BookingController();
+}
+/*
 
-            // Act
-           
+[TestMethod]
+public void Create([Bind(Include = "MeetingReference, RoomId, Date, Start_DateTime, End_DateTime")] Booking booking)
+{
+// Arrange
+BookingController bookingController = new BookingController();
 
-            // Assert
+// Act
 
-        }
-        [TestMethod]
-        public void Edit(int? id)
-        {
-           // Arrange
-           BookingController bookingController = new BookingController();
 
-            // Act
-           
+// Assert
 
-            // Assert
+}
+[TestMethod]
+public void Edit(int? id)
+{
+// Arrange
+BookingController bookingController = new BookingController();
 
-        }
-        [TestMethod]
-        public void EditPost(int? id)
-        {
-            // Arrange
-           BookingController bookingController = new BookingController();
+// Act
 
-            // Act
-           
 
-            // Assert
+// Assert
 
-        }
-        [TestMethod]
-        public void Delete(int? id, bool? saveChangesError = false)
-        {
-           // Arrange
-           BookingController bookingController = new BookingController();
+}
+[TestMethod]
+public void EditPost(int? id)
+{
+// Arrange
+BookingController bookingController = new BookingController();
 
-            // Act
-           
+// Act
 
-            // Assert
 
-        }
-        [TestMethod]
-        public void DeleteConfirmed(int id)
-        {
-           // Arrange
-           BookingController bookingController = new BookingController();
+// Assert
 
-            // Act
-           
+}
+[TestMethod]
+public void Delete(int? id, bool? saveChangesError = false)
+{
+// Arrange
+BookingController bookingController = new BookingController();
 
-            // Assert
+// Act
 
-        }
-        */
+
+// Assert
+
+}
+[TestMethod]
+public void DeleteConfirmed(int id)
+{
+// Arrange
+BookingController bookingController = new BookingController();
+
+// Act
+
+
+// Assert
+
+}
+*/
 
     }
 }
--- CA2_IMeet.Tests/Controllers/MeetingRoomsControllerTests.cs
+++ CA2_IMeet.Tests/Controllers/MeetingRoomsControllerTests.cs
@@ -7,109 +7,190 @@ namespace CA2_IMeet.Tests.Controllers
 {
 
     [TestClass]
-    public class MeetingRoomsControllerTests : MeetingRoomsController
+    public class MeetingRoomsControllerTests
 
     {
+        [TestMethod()]
+        public void IndexTest(string sortOrder, string searchString)
+        {/*
+            // Arrange
+            var meetroomController = new MeetingRoomsController();
+
+            var rooms = from r in db.MeetingRooms
+                        select r;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                rooms = rooms.Where(r => r.Name.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    rooms = rooms.OrderByDescending(r => r.Name);
+                    break;
+                case "Size":
+                    rooms = rooms.OrderBy(r => r.Size);
+                    break;
+                case "size_desc":
+                    rooms = rooms.OrderByDescending(r => r.Size);
+                    break;
+                default:
+                    rooms = rooms.OrderBy(r => r.Name);
+                    break;
+            }
+            return View(rooms.ToList());
+            // Action
+            var result = meetroomController.Index();
+            
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditPostTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
         /* Draft Meeting Room Contoller Tests
-        [TestMethod]
-        public void Index(string sortOrder, string searchString)
-        {
-            // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
+[TestMethod]
+public void Index(string sortOrder, string searchString)
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
 
-            // Act
+// Act
 
 
-            // Assert
+// Assert
 
+}
+
+[TestMethod]
+public void Details(string keyword)
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+[TestMethod]
+public void Create()
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+
+
+[TestMethod]
+public void Create([Bind(Include = "Name,Size,Location,Equipment")] Models.MeetingRoom meetingRoom)
+{
+
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+
+[TestMethod]
+public void Edit(int? id)
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+
+[TestMethod]
+public void EditPost(int? id)
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+
+[TestMethod]
+public void Delete(int? id, bool? saveChangesError = false)
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+
+[TestMethod]
+public void Delete(int id)
+{
+// Arrange
+MeetingRoomsController meetroomController = new MeetingRoomsController();
+
+// Act
+
+
+// Assert
+}
+
+*/
         }
-
-        [TestMethod]
-        public void Details(string keyword)
-        {
-            // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-        [TestMethod]
-        public void Create()
-        {
-            // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-
-
-        [TestMethod]
-        public void Create([Bind(Include = "Name,Size,Location,Equipment")] Models.MeetingRoom meetingRoom)
-        {
-
-            // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Edit(int? id)
-        {
-            // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void EditPost(int? id)
-        {
-           // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Delete(int? id, bool? saveChangesError = false)
-        {
-           // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-
-        [TestMethod]
-        public void Delete(int id)
-        {
-           // Arrange
-            MeetingRoomsController meetroomController = new MeetingRoomsController();
-
-            // Act
-
-
-            // Assert
-        }
-
-    */
     }
 }
+
 
