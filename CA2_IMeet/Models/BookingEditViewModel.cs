1342531c0d461560e72ac08b9e52394f6355c02e
Carole vd <Carole.vd@gmail.com>
2018-05-15 00:01:55 +0100

    Tiny Bits

--- CA2_IMeet/Models/BookingEditViewModel.cs
+++ CA2_IMeet/Models/BookingEditViewModel.cs
@@ -9,12 +9,6 @@ namespace CA2_IMeet.Models
 {
     public class BookingEditViewModel : BookingCreateViewModel
     {
-        //private DateTime start_DateTime;
-        //private DateTime end_DateTime;
-
-        //[HiddenInput]
-        //public int? BookingId { get; set; }
-
         [Required(ErrorMessage = "Indicate Meeting Reference.")]
         [StringLength(55, ErrorMessage = "Meeting Title cannot be longer than 55 characters.")]
         [Display(Name = "Meeting Title")]
@@ -23,54 +17,5 @@ namespace CA2_IMeet.Models
         [Required]
         [Display(Name = "Room")]
         public int RoomId { get; set; }
-
-        //[Required(ErrorMessage = "Indicate meeting date.")]
-        //[DataType(DataType.Date)]   
-        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        //[Weekdays]
-        //[InFuture]
-        //public DateTime Date { get; set; }
-
-        //[Required(ErrorMessage = "Indicate when meeting starts.")]
-        //[DataType(DataType.Time)]
-        //[Display(Name = "Start Time")]
-        //public DateTime Start_DateTime
-        //{
-        //    get
-        //    {
-        //        return start_DateTime;
-        //    }
-        //    set
-        //    {
-        //        start_DateTime = new DateTime(Date.Year, Date.Month, Date.Day, value.Hour, 0, 1);
-        //    }
-        //}
-
-        //[Required(ErrorMessage = "Indicate when meeting ends.")]
-        //[DataType(DataType.Time)]
-        //[Display(Name = "End Time")]
-        //public DateTime End_DateTime
-        //{
-        //    get
-        //    {
-        //        return end_DateTime;
-        //    }
-        //    set
-        //    {
-        //        end_DateTime = new DateTime(Date.Year, Date.Month, Date.Day, value.Hour, 0, 0);
-        //    }
-        //}
-
-        //public bool InvalidStartAndEnd()
-        //{
-        //    if (Start_DateTime >= End_DateTime)
-        //    {
-        //        return true;
-        //    }
-        //    else
-        //    {
-        //        return false;
-        //    }
-        //}
     }
 }
